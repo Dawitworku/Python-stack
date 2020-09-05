58823458aa2a8dd2069e4ccfba9e11ef742b7a2b
Dawitworku <dwc99519@marymount.edu>
2020-09-05 10:52:39 -0400

    Updated the Functions intermidiate2 Algo

--- _python/python_fundamentals/Functions_Intermediate2.py
+++ _python/python_fundamentals/Functions_Intermediate2.py
@@ -1,30 +1,31 @@
 # 1 Update Values in Dictionaries and Lists
-# x = [ [5,2,3], [10,8,9] ] 
-# students = [
-#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#      {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 20} ]
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
 
-# # A. Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
-# x[1] [0] = 15
-# print(x)
+# A. Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+x[1] [0] = 15
+print(x)
 
-# # B. Change the last_name of the first student from 'Jordan' to 'Bryant'
-# students[0] ['last_name'] = 'Bryant'
-# print(students)
+# B. Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0] ['last_name'] = 'Bryant'
+print(students)
 
-# # C. In the sports_directory, change 'Messi' to 'Andres'
-# sports_directory['soccer'] [0] = 'Andres'
-# print(sports_directory)
+# C. In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'] [0] = 'Andres'
+print(sports_directory)
 
-# # D. Change the value 20 in z to 30
-# z[0] ['y']= 30  # z is a list that has a dictionary inside of it.
-# print(z)
+# D. Change the value 20 in z to 30
+z[0] ['y']= 30  # z is a list that has a dictionary inside of it.
+print(z)
+
 #2 Iterate Through a List of Dictionaries
 students = [
          {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -33,27 +34,27 @@ students = [
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
 
-# def iterateDictionary(list):
+def iterateDictionary(list):
 #     # First method
-#     for i in list:                                   # iterates through the array
-#         for key,val in i.items():                    # iterates within the dictionary
-#             print(f"{key} - {val}")
-#     print("**********************")    
+    for i in list:                                   # iterates through the array
+        for key,val in i.items():                    # iterates within the dictionary
+            print(f"{key} - {val}")
+    print("**********************")    
 #     # Another way of doing this
-#     for i in list:                                   # iterates through the array
-#         str = ''
-#         for key,val in i.items():                    #  iterates within the dictionary
-#             str += f"{key} - {val}, "
-#         print(str)
-#     print("**********************")    
+    for i in list:                                   # iterates through the array
+        str = ''
+        for key,val in i.items():                    #  iterates within the dictionary
+            str += f"{key} - {val}, "
+        print(str)
+    print("**********************")    
 #     # Another way of doing this
-#     for i in list:                                   # iterates through the array
-#         str = '' 
-#         for j in i.keys(): 
-#             str += f"{j} - {i[j]}, "                 #grabs and concatnates key: value in each "i" iteration
-#         print(str)
+    for i in list:                                   # iterates through the array
+        str = '' 
+        for j in i: 
+            str += f"{j} - {i[j]}, "                 #grabs and concatnates key: value in each "i" iteration
+        print(str)
 # # To be able to print the key & values same way on the list, we have to store the value of each iteration inside of a varable.            
-# iterateDictionary(students)
+iterateDictionary(students)
 
 # 3 Get Values From a List of Dictionaries
 def iterateDictionary2(key_name, some_list):
@@ -67,6 +68,7 @@ def iterateDictionary2(key_name, some_list):
 iterateDictionary2('first_name', students) 
 print("**********************")
 iterateDictionary2('last_name', students) 
+
 # 4 Iterate Through a Dictionary with List Values
 dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
@@ -89,8 +91,8 @@ def printInfo(some_dict):
 
     for i in some_dict.keys():   # .keys method will help me wit grabing the keys of the dictionary
         print(f"{len(some_dict[i])} {i.upper()}")
-        for i in some_dict[i]:
-            print(i)
+        for j in some_dict[i]:
+            print(j)
         print("\n")
         
 printInfo(dojo)
