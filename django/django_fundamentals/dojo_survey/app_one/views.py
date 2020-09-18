f9c264c175b49a5077e855d2dfce65fb96a3679c
Dawitworku <dwc99519@marymount.edu>
2020-09-18 11:10:39 -0400

    Dojo Survey Revisited

--- django/django_fundamentals/dojo_survey/app_one/views.py
+++ django/django_fundamentals/dojo_survey/app_one/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+
 def index(request):
     return render(request,'index.html')
 
 def submission(request):
-
     if request.method == "POST":
-        context = {
-            "name" : request.POST['name'],
-            "location" : request.POST['location'],
-            "language" : request.POST['language'],
-            "comment" : request.POST['comment'],
-        }
-        #print(request.POST)
-        return redirect('/')
+        # context = {
+        #     "name" : request.POST['name'],
+        #     "location" : request.POST['location'],
+        #     "language" : request.POST['language'],
+        #     "comment" : request.POST['comment'],
+        # }
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['language'] = request.POST['language']
+        request.session['comment'] = request.POST['comment']
+
+        print("This is post**********************")
+        return redirect('/result')
+        
     #     return render(request, 'results.html', context)
     # return render(request, 'results.html')
 
+def results(request):
+    return render(request, 'results.html')
